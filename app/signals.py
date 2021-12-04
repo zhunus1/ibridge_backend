7febd8bc8c21de27c89acdc76ecc20f268bc2749
zhunus1 <tengri.07@mail.ru>
2021-12-04 11:58:10 +0600

    'Fix'

--- app/signals.py
+++ app/signals.py
@@ -14,7 +14,6 @@ bot_token = settings.TELEGRAM_BOT_API
 def send_telegram(message):
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=-1001150175962' + '&parse_mode=Markdown&text=' + message
     response = requests.get(send_text)
-    return response.status_code
 
 def send_yandex(message):
     email = EmailMessage('Форма обратной связи', message, to=['admissions@ibridge.kz'])
@@ -22,13 +21,10 @@ def send_yandex(message):
 
 
 def send_bitrix(params):
+    url = 'https://ibridge.bitrix24.kz/rest/1/40df1gu85r4s1itm/crm.lead.add.json'
     if 'FIELDS[COMMENTS]' in params:
         url = 'https://ibridge.bitrix24.kz/rest/1/hvtvs3w97wbm6jm2/crm.lead.add.json'
-    else:
-        url = 'https://ibridge.bitrix24.kz/rest/1/40df1gu85r4s1itm/crm.lead.add.json'
     response = requests.post(url, params=params)
-    send_yandex(response.status_code)
-    return response.status_code
 
 
 @receiver(post_save, sender=Form)
