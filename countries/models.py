837d1b3bdb22cf55b3b9512f6b7745e9a22d69e0
zhunus1 <tengri.07@mail.ru>
2022-02-05 11:25:40 +0600

    Added model countries

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.utils.translation import *
 
 # Create your models here.
+class AboutImage(models.Model):
+    image = models.ImageField(upload_to ='countries/about/')
+    
+    created = models.DateTimeField(
+        verbose_name = ugettext_lazy("Created"),
+        auto_now_add = True,
+    )
+
+    updated = models.DateTimeField(
+        verbose_name = ugettext_lazy("Updated"),
+        auto_now = True,
+    )
+
+    class Meta:
+
+        verbose_name = ugettext_lazy("About Image")
+        verbose_name_plural = ugettext_lazy("About Images")
+        ordering = ('-created',)
+
+class Country(models.Model):
+    country_name = models.CharField(
+        max_length=255,
+    )
+
+    banner_title = models.CharField(
+        max_length=255,
+    )
+    
+    banner_sub_title = models.TextField(
+        blank=True,
+    )
+
+    banner_image = models.ImageField(upload_to ='countries/banner/')
+
+    about_text = models.TextField(
+        blank=True,
+    )
+
+    about_image = models.ManyToManyField(
+        to = AboutImage,
+        related_name='countries'
+    )
+
+    advantage_1 = models.TextField(
+        blank=True,
+    )
+
+    advantage_2 = models.TextField(
+        blank=True,
+    )
+
+    advantage_3 = models.TextField(
+        blank=True,
+    )
+
+    advantage_4 = models.TextField(
+        blank=True,
+    )
+
+    created = models.DateTimeField(
+        verbose_name = ugettext_lazy("Created"),
+        auto_now_add = True,
+    )
+
+    updated = models.DateTimeField(
+        verbose_name = ugettext_lazy("Updated"),
+        auto_now = True,
+    )
+
+    class Meta:
+
+        verbose_name = ugettext_lazy("Country")
+        verbose_name_plural = ugettext_lazy("Countries")
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.country_name
--- ibridge_backend/settings/development.py
+++ ibridge_backend/settings/development.py
@@ -7,7 +7,11 @@ DEBUG = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        'USER': 'postgres',
+        'PASSWORD': 'Behappy7+',
+        'HOST': env('DB_HOST'),
+        'PORT': '5432',
     }
 }
