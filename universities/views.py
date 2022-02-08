716bacf1b77685191f34070bcd9c0c286d24d748
zhunus1 <tengri.07@mail.ru>
2022-02-08 15:00:05 +0600

    finished partners api

--- universities/views.py
+++ universities/views.py
@@ -11,19 +11,27 @@ from .serializers import (
 
 
 class PartnerViewSet(viewsets.ReadOnlyModelViewSet):
-    pass
     
-    # def get_serializer_class(self):
-    #     if self.action == 'list':
-    #         return PartnerListSerializer
-    #     return PartnerDetailSerializer
-    
-    # def get_queryset(self):
-    #     queryset = Partner.objects.all()
-    #     country = self.request.query_params.get('country', None)
-    #     partner_type = self.request.query_params.get('type', None)
-    #     if country is not None and partner_type is not None:
-    #         queryset = queryset.filter(country__country_name=country, partner_translations__partner_type__title=partner_type)
-    #     return queryset
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PartnerListSerializer
+        return PartnerDetailSerializer
 
-    #filter by country and type
+    def get_queryset(self):
+        queryset = Partner.objects.all()
+        if self.action == 'list':
+            queryset = Partner.objects \
+                    .prefetch_related('partner_translations') \
+                    .order_by('created') \
+                    .all()
+        return queryset
+
+    def filter_queryset(self, queryset):
+        if self.action == 'list':
+            serializer = PartnerFilterSerializer(data=self.request.query_params)
+            serializer.is_valid(raise_exception=True)
+            queryset = queryset.filter(
+                partner_translations__partner_type__title=serializer.validated_data['partner_type'],
+                country__country_name=serializer.validated_data['country']
+            )
+        return super().filter_queryset(queryset)
