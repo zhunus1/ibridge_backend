b0219a5d70c3437141c6f3ad2b70993b200eb91c
zhunus1 <tengri.07@mail.ru>
2022-09-01 12:48:21 +0600

    Added slug filter

--- universities/filters.py
+++ universities/filters.py
@@ -4,10 +4,10 @@ from .models import Partner
 class PartnerFilter(filters.FilterSet):
     title = filters.CharFilter(field_name='partner_translations__partner_type__title')
     country_name = filters.CharFilter(field_name='country__country_name')
-
     class Meta:
         model = Partner
         fields = (
             'title',
-            'country_name'
+            'country_name',
+            'partner_slug'
         )
