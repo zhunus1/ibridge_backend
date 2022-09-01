e217c01a1972c2083da3eff736ba488dffc58e7e
zhunus1 <tengri.07@mail.ru>
2022-09-01 13:03:57 +0600

    Fixed filter

--- universities/filters.py
+++ universities/filters.py
@@ -1,13 +1,19 @@
 import django_filters as filters
 from .models import Partner
+import django_filters
 
 class PartnerFilter(filters.FilterSet):
     title = filters.CharFilter(field_name='partner_translations__partner_type__title')
-    country_name = filters.CharFilter(field_name='country__country_name')
+    q = django_filters.CharFilter(method='my_custom_filter', label="country_name")
+
     class Meta:
         model = Partner
         fields = (
             'title',
-            'country_name',
-            'partner_slug'
+            'q',
+        )
+    
+    def my_custom_filter(self, queryset, name, value):
+        return queryset.filter(
+            Q(country__country_name=value) | Q(country__country_slug=value)
         )
